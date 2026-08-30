cd69d717e174616a2c750a3e2b39a2a29e41a47f
mchaudhry02 <mchaudhry20@hotmail.com>
2026-08-30 16:20:08 -0500

    Add MovieManagerApplication.java.java

---
+++ movie-manage-group/src/main/java/com/launchcode/moviemanager/MovieManagerApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.moviemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieManagerApplication.class, args);
+    }
+
+}
